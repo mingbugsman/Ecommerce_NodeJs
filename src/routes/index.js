fc02b167cfe18a712a58b20952e89c983bcf7055
Tran Tuan Minh <ming18380@gmail.com>
2024-07-17 22:46:39 +0700

    add checkAuth and apiKeyService
    bổ sung kiểm tra apiKey và permissions trong checkAuth và findById trong apikey.service để hỗ trợ tìm key của apiKey trong dbs

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,14 @@
 'use strict'
 
 const express = require('express');
+const { apiKey, checkPermission } = require('../auth/checkAuth');
 const router = express.Router();
 
-// CHECK API
-
-
+// CHECK APIKEY
+router.use(apiKey);
 
 //CHECK PERMISSION CÓ ĐƯỢC TRUY CẬP VÀO HỆ THỐNG BACKEND CỦA TA HAY KHÔNG ???
-
+router.use(checkPermission('0000'))
 
 // SAU KHI CHECK THÌ CHECK CHO NGƯỜI DÙNG
 router.use('/v1/api', require('./access'))
